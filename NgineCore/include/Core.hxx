2e83ce7605b3a6dec113f5252bdcf0dcf44f8393
Zbigniew Przybyła <zprzybyla@tuta.io>
2024-12-23 21:36:43 +0100

    Add graphics core class for Win32 platform

--- NgineCore/include/Core.hxx
+++ NgineCore/include/Core.hxx
@@ -30,6 +30,9 @@
 //Loguru headers
 #include <loguru.hpp>
 
+#define DX_THROW_IF_FAILED(hr) if(FAILED(hr)) throw DirectXException(hr)
+#define DX_COM_RELEASE(p) if(p != nullptr) p->Release()
+
 #elif defined(TARGET_PLATFORM_LINUX)
 
 #define VK_USE_PLATFORM_XLIB_KHR
